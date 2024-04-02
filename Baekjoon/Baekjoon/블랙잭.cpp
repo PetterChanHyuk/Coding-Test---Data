8f080e673122103241449cf6885cd4a77e11f153
PetterChanHyuk <pchishahaboy@naver.com>
2024-04-02 18:22:39 +0900

    add

---
+++ Baekjoon/Baekjoon/블랙잭.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N, M;
+
+	cin >> N >> M;
+
+	vector<int> T(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> T[i];
+	}
+
+	int max=0;
+	for (int j = 0; j < N-2; j++)
+	{
+		for (int k = j+1; k < N-1; k++)
+		{
+			for (int l = k + 1; l < N; l++)
+			{
+				if (max < T[j] + T[k] + T[l] && T[j] + T[k] + T[l] <= M)
+					max = T[j] + T[k] + T[l];
+			}
+		}
+	}
+	cout << max;
+}
